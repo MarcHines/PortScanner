333ea9ec57bb275c03893f74414a03ef82889a32
MarcHines <mxh857@miami.edu>
2017-12-04 09:38:51 -0500

    Update application.java

--- application.java
+++ application.java
@@ -2,7 +2,7 @@ public class application {
 
 	public static void main(String[] args) throws Exception {
 		
-		PortScanner portscanner = new PortScanner("meclabs.com");
+		PortScanner portscanner = new PortScanner("example.com");
 		portscanner.findOpenPorts(1, 100);
 	}
 
